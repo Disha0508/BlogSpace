96c0960ce809a71b42e5ba80b8acf508a00adb1a
Diana <Dianasha2006@gmail.com>
2026-07-21 20:34:15 +0300

    Update gitattributes

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
 
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 
   transform: {
-    "^.+\\.(js|jsx)$": [
-      "babel-jest",
+    '^.+\\.(js|jsx)$': [
+      'babel-jest',
       {
         presets: [
-          ["@babel/preset-env", { targets: { node: "current" } }],
-          ["@babel/preset-react", { runtime: "automatic" }],
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          ['@babel/preset-react', { runtime: 'automatic' }],
         ],
       },
     ],
   },
 
-  transformIgnorePatterns: ["/node_modules/(?!(@testing-library)/)"],
+  transformIgnorePatterns: ['/node_modules/(?!(@testing-library)/)'],
 };
